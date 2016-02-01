ca8088a979f926560cc2e7f124581b58ba77d016
ssadhoo <sarvesh_sadhoo@intuit.com>
2016-02-01 15:36:39 -0800

    Intial commit

---
+++ server.js
@@ -0,0 +1,153 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+var morgan = require('morgan');
+
+var db = require('./db.js');
+var app = express();
+
+var PORT = process.ENV.PORT || 3000;
+var ENV = process.ENV.NODE_ENV || 'development';
+
+if (PORT == 3000) {
+	app.use(morgan('common'));
+}
+
+app.use(bodyParser.json());
+
+
+//GET /
+app.get('/', function(req, res) {
+	res.send('Todo API Root');
+});
+
+
+// GET /todos/:id
+app.get('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	db.todo.findById(todoId).then(function(todo) {
+		if (todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function(e) {
+		res.status(500).send();
+	});
+});
+
+
+// GET /todos?completed=false&q=work
+app.get('/todos', function(req, res) {
+	var query = req.query;
+	var where = {};
+
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
+	}
+
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		if (ENV === 'production') {
+			where.description = {
+				$iLike: '%' + query.q + '%'
+			}
+		} else {
+			where.description = {
+				$like: '%' + query.q + '%'
+			}
+		}
+
+	}
+
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
+		res.json(todos);
+	}, function(e) {
+		res.status(500).send();
+	});
+});
+
+
+// POST /todos
+app.post('/todos', function(req, res) {
+	var body = _.pick(req.body, 'description', 'completed');
+	if (typeof body.completed !== 'boolean') {
+		return res.status(400).json({
+			message: 'invalid completed type. "completed is of type boolean'
+		});
+	}
+
+	db.todo.create(body).then(function(todo) {
+		res.json(todo.toJSON());
+	}, function(e) {
+		res.status(400).json(e);
+	});
+});
+
+
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var body = _.pick(req.body, 'description', 'completed');
+	var options = {
+		where: {
+			id: todoId
+		}
+	}
+
+	if (ENV === 'production') { // Setting Postgres database to return affected row
+		options.returning = true; 
+	}
+
+	db.todo.update(body, options).then(function(updatedRows) {
+		if (updatedRows[0] === 0) {
+			return res.status(404).send();
+		} else if (updatedRows[0] === 1) {
+			if (ENV === 'production') { // Postgres database returns affected rows. Sqlite only returns no.of affected rows
+				var updatedTodo = updatedRows[1][0].dataValues;
+				return res.json(updatedTodo);
+			} else {
+				return res.json({
+					message: "database is sqlite",
+					rowsAffected: updatedRows[0]
+				});
+			}
+
+		}
+	}, function(e) {
+		res.status(400).json(e);
+	});
+});
+
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function() {
+		res.status(500).send();
+	});
+});
+
+
+
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on PORT ' + PORT + '!');
+	});
+});
