8225552de8abce83d14a204ca213c7e897dec0b2
srv89 <sarveshsadhoo@gmail.com>
2016-02-02 13:33:12 -0800

    Added hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function(sequelize, DateTypes) {
 	return sequelize.define('user', {
 		email: {
@@ -8,20 +11,40 @@ module.exports = function(sequelize, DateTypes) {
 				isEmail: true
 			}
 		},
+		salt: {
+			type: DateTypes.STRING
+		},
+		hahed_password: {
+			type: DateTypes.STRING
+		},
 		password: {
-			type: DateTypes.STRING,
+			type: DateTypes.VIRTUAL,
 			allowNull: false,
 			validate: {
 				len: [7, 100]
+			},
+			set: function(value) {
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
+
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword);
 			}
-		},
+		}
 	}, {
 		hooks: {
-				beforeValidate: function(user, option) {
-					if (typeof user.email === 'string') {
-						user.email = user.email.toLowerCase();
-					}
+			beforeValidate: function(user, option) {
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
 				}
 			}
+		},
+		instanceMethods: {
+			toPublicJSON: function () {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt');
+			}
+		}
 	});
 }
--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 
 	}, function(e) {
 		res.status(400).json(e);
@@ -161,7 +161,7 @@ app.post('/users', function(req, res) {
 
 })
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force:true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on PORT ' + PORT + '!');
 	});
