f44e0814f3274e44fe1079c873f8a9a75e36dd0f
srv89 <sarveshsadhoo@gmail.com>
2016-02-09 16:35:58 -0800

    Added POST /users/logout and token.js

--- server.js
+++ server.js
@@ -181,21 +181,32 @@ app.post('/users', function(req, res) {
 
 
 // POST (Path: /users/login)
-app.post("/users/login", function(req, res) {
+app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
-	db.user.authenticate(body).then(function(user) {
+	db.user.authenticate(body).then(function (user) {
 		var token = user.generateToken('authentication');
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			return res.status(401).send();
-		}
+		userInstance = user;
 
-	}, function(e) {
+		return db.token.create({
+			token: token
+		});
+	}).then(function (tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function () {
 		res.status(401).send();
 	});
+});
 
+
+// DELETE (Path: /users/logout)
+app.delete('/users/logout', middleware.requireAuthentication, function (req, res) {
+	req.token.destroy().then(function () {
+		res.status(204).send();
+	}).catch(function () {
+		res.status(500).send();
+	})
 });
 
 
