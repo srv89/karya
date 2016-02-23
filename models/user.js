e52b1da9d6ba2a40332af0f42b7be6b70952684f
srv89 <sarveshsadhoo@gmail.com>
2016-02-22 16:24:18 -0800

    SENDGRID_API_KEY import issue solved

--- models/user.js
+++ models/user.js
@@ -2,10 +2,14 @@ var bcrypt = require('bcrypt');
 var _ = require('underscore');
 var cryptojs = require('crypto-js');
 var jwt = require('jsonwebtoken');
+
 var ENV = process.env.NODE_ENV || 'development';
 
 if (ENV === "development") {
 	var config = require('../.env/config.js');
+	var SENDGRID_API_KEY = config.SENDGRID_API_KEY
+} else {
+	var SENDGRID_API_KEY = process.env.SENDGRID_API_KEY
 };
 
 
