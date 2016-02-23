e03d9ed10e333a9ab5031763a6c796861bb93cb2
srv89 <sarveshsadhoo@gmail.com>
2016-02-22 17:56:48 -0800

    send grid api key issue

--- password_manager/sendgridService.js
+++ password_manager/sendgridService.js
@@ -2,9 +2,9 @@ var ENV = process.env.NODE_ENV || 'development';
 
 if (ENV === "development") {
 	var config = require('../.env/config.js');
-	var SENDGRID_API_KEY = config.SENDGRID_API_KEY
+	var SENDGRID_API_KEY = config.SENDGRID_API_KEY;
 } else {
-	var SENDGRID_API_KEY = process.env.SENDGRID_API_KEY
+	var SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 };
 
 console.log( "SENDGRID_API_KEY: ", SENDGRID_API_KEY);
